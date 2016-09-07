a5a8265d108af1ba5b1a56183af62a03ce6e14a6
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-09-07 15:40:21 +0900

    Don't show revision

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	cmdName := args[0]
 	cmdArgs := args[1:]
 
-	info(fmt.Sprintf("Version: %s , Revision: %s", Version, GitCommit))
+	info(fmt.Sprintf("Version: %s", Version))
 	info(fmt.Sprintf("Running command: %s %s", cmdName, strings.Join(cmdArgs, " ")))
 
 	exitStatus, execErr := herokuDeployWrap(cmdName, cmdArgs, os.Stdin, os.Stdout, os.Stderr)
