e56a9070091442e6604e9a65ed986ff4a20a5a64
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-09-07 15:44:02 +0900

    Bump

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 // Version is displayed in help command
-const Version string = "0.0.5"
+const Version string = "0.0.6"
 
 // GitCommit is commit hash of the release
 // This is set in compilation automatically
